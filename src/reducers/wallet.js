b31113d6ed836fb365d7daa16383234c45f7cde4
Hanna Bacelar <hannabacelar@hotmail.com>
2022-04-01 20:04:55 -0300

    requisito 1 e 2 tirando erro de lint

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,17 +1,17 @@
-import { ACTION_CURRENCIES } from '../actions';
+// // import { ACTION_CURRENCIES } from '../actions';
 
-const INITIAL_STATE = {
-  email: '',
-};
+// const INITIAL_STATE = {
+//   email: '',
+// };
 
-function user(state = INITIAL_STATE, action) {
-//   switch (action.type) {
-//   case ACTION_EMAIL:
-//     return {
-//       ...state, email: action.email,
-//     };
-//   default:
-  return state;
-//   }
-}
-export default user;
+// // function user(state = INITIAL_STATE, action) {
+// //   switch (action.type) {
+// //   case ACTION_EMAIL:
+// //     return {
+// //       ...state, email: action.email,
+// //     };
+// //   default:
+//   return state;
+// //   }
+// // }
+// export default user;
