95c3759e6cf53dc108e26d99a5a01710d70fb654
Hanna Bacelar <hannabacelar@hotmail.com>
2022-04-01 22:30:55 -0300

    requisito 3

---
+++ src/pages/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+export class Header extends React.Component {
+  render() {
+    const { emailState } = this.props;
+    return (
+      <header>
+        <p
+          data-testid="email-field"
+        >
+          Email usuário:
+          { emailState }
+        </p>
+        <p
+          data-testid="total-field"
+        >
+          0
+        </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  emailState: state.user.email,
+});
+
+Header.propTypes = {
+  emailState: PropTypes.string,
+}.isRequired;
+
+// mapStateToProps é uma função que você usaria para fornecer os dados da loja ao seu componente,
+export default connect(mapStateToProps)(Header);
